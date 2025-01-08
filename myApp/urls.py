3f9a2937a15774e8c800277bd9f426dc1d288d26
rubendario921 <rubendario199201@gmail.com>
2025-01-07 23:21:51 -0500

    update changes for ulrs

--- myApp/urls.py
+++ myApp/urls.py
@@ -20,6 +20,6 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.helloWord),
-    path('signup/', views.helloWord),
+    path('', views.home, name="home"),
+    path('signup/', views.signup, name="signup"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def helloWord(request):
-    return render(request,'signup.html',{'form':UserCreationForm()})
+def home (request):
+   return render (request,'home.html')
+def signup(request):
+   return render(request,'signup.html',{'form':UserCreationForm()})
